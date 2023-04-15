a8389b004711932428567c487bd42220dae76a9d
Xhofe <Xhofe@users.noreply.github.com>
2023-04-15 14:26:56 +0000

    deploy: bc43e11b72aefc8f1dc07ba2a251b177ea97ac31

--- assets/mermaid-mindmap.esm.min-6ff69cec.js
+++
@@ -1,1 +0,0 @@
-import{aG as r}from"./app-56e27fb3.js";import"./framework-95cb4087.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-4dda171c.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};
