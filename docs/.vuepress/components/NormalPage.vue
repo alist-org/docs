0595275c2ce6a3888f4b39df24c3e79c7b81f1ed
Andy Hsu <i@nn.ci>
2023-04-26 14:32:03 +0800

    fix sponsor style

--- docs/.vuepress/components/NormalPage.vue
+++ docs/.vuepress/components/NormalPage.vue
@@ -2,7 +2,7 @@
   <NormalPage>
     <template #contentBefore>
       <div class="theme-hope-content ads-container">
-        <div class="mingdao" v-if="!enableSidebar">
+        <div class="mingdao" v-if="showMingdao">
           <a href="https://www.mingdao.com?s=utm_51=utm_source=liteflow&utm_medium=banner&utm_campaign=%E5%93%81%E7%89%8C%E6%8E%A8%E5%B9%BF&utm_content=IT%E8%B5%8B%E8%83%BD%E4%B8%9A%E5%8A%A1"
             target="_blank">
             <img src="/img/ads/mingdao-h.png" alt="" />
@@ -10,6 +10,12 @@
           </a>
         </div>
         <div class="wwads wwads-cn wwads-horizontal" data-id="213" style="width:100% !important" v-else>
+        </div>
+      </div>
+    </template>
+    <template #contentAfter>
+      <div class="bottom-wwads">
+        <div class="wwads wwads-cn wwads-horizontal" data-id="213" style="width:90% !important" v-if="showMingdao">
         </div>
       </div>
     </template>
@@ -25,8 +31,12 @@ const pageData = usePageData()
 
 // get is enable sidebar from frontmatter
 const frontmatter = usePageFrontmatter();
-const enableSidebar = computed(() => {
-  return frontmatter.value.sidebar === undefined ? true : frontmatter.value.sidebar;
+const showMingdao = computed(() => {
+  if (pageData.value.path.startsWith("/tool/")) {
+    return true;
+  }
+  const sidebar = frontmatter.value.sidebar;
+  return sidebar === false;
 })
 
 const spStr = computed(() => {
@@ -61,6 +71,11 @@ const spStr = computed(() => {
   }
 }
 
+.bottom-wwads {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
 .ads-container {
   padding-top: 0 !important;
   padding-bottom: 0 !important;
