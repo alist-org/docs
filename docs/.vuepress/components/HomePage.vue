2f1c629ba75bd6fe3ed84f98e40eb1b3d58b2b79
Andy Hsu <i@nn.ci>
2023-04-27 20:21:27 +0800

    change mingdao link

--- docs/.vuepress/components/HomePage.vue
+++ docs/.vuepress/components/HomePage.vue
@@ -4,7 +4,7 @@
       <div class="ss">
         <span>🌈 {{ spStr }}</span>
         <div class="mingdao">
-          <a href="https://www.mingdao.com?s=utm_51=utm_source=liteflow&utm_medium=banner&utm_campaign=%E5%93%81%E7%89%8C%E6%8E%A8%E5%B9%BF&utm_content=IT%E8%B5%8B%E8%83%BD%E4%B8%9A%E5%8A%A1"
+          <a href="https://www.mingdao.com?s=utm_51=utm_source=alist&utm_medium=banner&utm_campaign=%E5%93%81%E7%89%8C%E6%8E%A8%E5%B9%BF&utm_content=IT%E8%B5%8B%E8%83%BD%E4%B8%9A%E5%8A%A1"
             target="_blank"><img src="/img/ads/mingdao-h.png" alt="" /></a>
         </div>
       </div>
