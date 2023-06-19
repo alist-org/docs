832b3236451066cadf5af0e59d1668b38b1f1ca9
Andy Hsu <i@nn.ci>
2023-06-19 23:02:49 +0800

    dropbox tool

--- docs/.vuepress/components/api/ApiSelect.vue
+++ docs/.vuepress/components/api/ApiSelect.vue
@@ -1,19 +1,30 @@
 <script setup lang="ts">
-import { NSelect } from 'naive-ui';
-import { computed, ref, watch } from 'vue';
-import { apis, defaultApi } from '.';
-import NaiveClient from '../NaiveClient.vue';
+import { NSelect } from "naive-ui";
+import { computed, ref, watch } from "vue";
+import { apis, defaultApi } from ".";
+import NaiveClient from "../NaiveClient.vue";
 
-const cur_api_name = ref(defaultApi)
-const api = computed(() => apis[cur_api_name.value] as string)
-watch(api, (v) => {
-  localStorage.setItem("api", v)
-})
-const api_options = Object.entries(apis).map(([key, value]) => ({ label: `${value} (${key})`, value: key }))
+const cur_api_name = ref(defaultApi);
+if (typeof localStorage !== "undefined") {
+  cur_api_name.value = localStorage.getItem("api_name") || defaultApi;
+}
+
+watch(cur_api_name, (v) => {
+  localStorage.setItem("api_name", v);
+});
+const api_options = Object.entries(apis).map(([key, value]) => ({
+  label: `${value} (${key})`,
+  value: key,
+}));
 </script>
 
 <template>
   <NaiveClient>
-    <NSelect v-model:value="cur_api_name" size="large" :options="api_options" style="margin-bottom: 4px;"></NSelect>
+    <NSelect
+      v-model:value="cur_api_name"
+      size="large"
+      :options="api_options"
+      style="margin-bottom: 4px"
+    ></NSelect>
   </NaiveClient>
 </template>
---
+++ docs/tool/dropbox/request.md
@@ -0,0 +1,12 @@
+---
+title: "Get Dropbox Refresh Token"
+toc: false
+---
+
+<NaiveClient>
+<Dropbox />
+</NaiveClient>
+
+<script setup lang="ts">
+import Dropbox from "@Dropbox/Request";
+</script>
