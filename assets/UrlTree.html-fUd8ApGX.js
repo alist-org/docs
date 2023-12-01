d3669731eeb2446e38e3867674daf57ae26ea52f
xhofe <xhofe@users.noreply.github.com>
2023-12-01 08:17:02 +0000

    deploy: 71080b8dae44e46db9b533a466233aa49735fdde

---
+++ assets/UrlTree.html-fUd8ApGX.js
@@ -0,0 +1,115 @@
+import{_ as u,r as d,o as m,c as b,b as e,e as t,a as l,w as n,f as h}from"./app-ThyR28W4.js";const p={},v=e("h1",{id:"urltree",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#urltree","aria-hidden":"true"},"#"),t(" UrlTree")],-1),g={class:"hint-container tip"},f=e("p",{class:"hint-container-title"},"Tips",-1),j=e("strong",null,[e("code",null,"UrlTree")],-1),_={href:"https://github.com/alist-org/alist/issues/3268",target:"_blank",rel:"noopener noreferrer"},x=e("h2",{id:"usage",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#usage","aria-hidden":"true"},"#"),t(),e("strong",null,"usage")],-1),w=e("p",null,"Let me briefly explain the specific usage. It is recommended to look at the different usages below. The more advanced the gameplay",-1),y=e("p",null,"Don't look at the writing, it looks difficult and long-winded, but in fact, you can understand it in seconds if you operate it yourself.",-1),k=e("h5",{id:"input",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#input","aria-hidden":"true"},"#"),t(),e("strong",null,"input")],-1),A=e("div",{class:"language-text line-numbers-mode","data-ext":"text"},[e("pre",{class:"language-text"},[e("code",null,`http://pic.rmb.bdstatic.com/bjh/aff9c9c8817d943d6857a73188958d45.jpeg
+https://raw.githubusercontent.com/alist-org/alist/main/README_cn.md
+`)]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),E=e("h5",{id:"output",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#output","aria-hidden":"true"},"#"),t(),e("strong",null,"output")],-1),T=e("div",{class:"language-text line-numbers-mode","data-ext":"text"},[e("pre",{class:"language-text"},[e("code",null,`│ aff9c9c8817d943d6857a73188958d45.jpeg
+│ README_cn.md
+`)]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),I=e("p",null,null,-1),D=e("h5",{id:"input-1",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#input-1","aria-hidden":"true"},"#"),t(),e("strong",null,"input")],-1),M=e("div",{class:"language-text line-numbers-mode","data-ext":"text"},[e("pre",{class:"language-text"},[e("code",null,`http://pic.rmb.bdstatic.com/bjh/aff9c9c8817d943d6857a73188958d45.jpeg
+folder:
+  http://pic.rmb.bdstatic.com/bjh/aff9c9c8817d943d6857a73188958d45.jpeg
+  https://raw.githubusercontent.com/alist-org/alist/main/README_cn.md
+`)]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),R=e("div",{class:"language-text line-numbers-mode","data-ext":"text"},[e("pre",{class:"language-text"},[e("code",null,`http://pic.rmb.bdstatic.com/bjh/aff9c9c8817d943d6857a73188958d45.jpeg
+folder:
+  http://pic.rmb.bdstatic.com/bjh/aff9c9c8817d943d6857a73188958d45.jpeg
+  https://raw.githubusercontent.com/alist-org/alist/main/README_cn.md
+http://pic.rmb.bdstatic.com/bjh/a66923e9bfefd03c2998ee998d3b55e7.jpeg
+folde2r:
+  http://pic.rmb.bdstatic.com/bjh/e919290bf3b8396817d369cedf33b8cd.jpeg
+  https://raw.githubusercontent.com/alist-org/alist/main/CONTRIBUTING.md
+`)]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),B=e("h5",{id:"output-1",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#output-1","aria-hidden":"true"},"#"),t(),e("strong",null,"output")],-1),z=e("div",{class:"language-text line-numbers-mode","data-ext":"text"},[e("pre",{class:"language-text"},[e("code",null,`│  aff9c9c8817d943d6857a73188958d45.jpeg
+└─folder
+        aff9c9c8817d943d6857a73188958d45.jpeg
+        README_cn.md
+`)]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),U=e("div",{class:"language-text line-numbers-mode","data-ext":"text"},[e("pre",{class:"language-text"},[e("code",null,`│  	aff9c9c8817d943d6857a73188958d45.jpeg
+├─folder
+│      aff9c9c8817d943d6857a73188958d45.jpeg
+│      README_cn.md
+│
+├─	a66923e9bfefd03c2998ee998d3b55e7.jpeg
+│
+└─folder2
+        CONTRIBUTING.md
+        e919290bf3b8396817d369cedf33b8cd.jpeg
+`)]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),N=e("p",null,"There must be two blank spaces at the beginning~",-1),S=e("p",null,null,-1),C=e("p",null,[t("Format: "),e("code",null,"new name.suffix:file size:link")],-1),O=e("div",{class:"language-text line-numbers-mode","data-ext":"text"},[e("pre",{class:"language-text"},[e("code",null,`new namejpeg:1024:http://pic.rmb.bdstatic.com/bjh/aff9c9c8817d943d6857a73188958d45.jpeg
+new name2.md:2048:https://raw.githubusercontent.com/alist-org/alist/main/README_cn.md
+`)]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),W=e("p",null,"The size of the file is not written, and it can also be renamed",-1),F=e("div",{class:"language-text line-numbers-mode","data-ext":"text"},[e("pre",{class:"language-text"},[e("code",null,`new name.jpeg:1024:http://pic.rmb.bdstatic.com/bjh/aff9c9c8817d943d6857a73188958d45.jpeg
+new name2.md:2048:https://raw.githubusercontent.com/alist-org/alist/main/README_cn.md
+`)]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),Q=e("p",null,null,-1),V=e("ul",null,[e("li",null,"The first-level directory folder does not need spaces, but the files in the first-level directory need to be empty with two spaces.")],-1),Y=e("div",{class:"language-text line-numbers-mode","data-ext":"text"},[e("pre",{class:"language-text"},[e("code",null,`folder:
+  http://pic.rmb.bdstatic.com/bjh/aff9c9c8817d943d6857a73188958d45.jpeg
+  https://raw.githubusercontent.com/alist-org/alist/main/README_cn.md
+`)]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),G=e("ul",null,[e("li",null,[t("If you have written the first layer and want to put another one in the first folder, then put a layer under this folder, the writing method is as follows, very simple "),e("ul",null,[e("li",null,"The directory is empty with two spaces, and the file is four if you add two spaces. If you continue to want to go down one level and continue to add 2 spaces")])])],-1),K=e("div",{class:"language-text line-numbers-mode","data-ext":"text"},[e("pre",{class:"language-text"},[e("code",null,`folder11:
+  http://pic.rmb.bdstatic.com/bjh/111.jpeg
+  folder111:
+    http://pic.rmb.bdstatic.com/bjh/111.jpeg
+    folder1111:
+      http://pic.rmb.bdstatic.com/bjh/111.jpeg
+  folder222:
+    http://pic.rmb.bdstatic.com/bjh/222.jpeg
+    folder2222:
+      http://pic.rmb.bdstatic.com/bjh/222.jpeg
+folder22:
+  http://pic.rmb.bdstatic.com/bjh/222.jpeg
+  folder222:
+    http://pic.rmb.bdstatic.com/bjh/222.jpeg
+    folder2222:
+      http://pic.rmb.bdstatic.com/bjh/2223.jpeg
+folder33:
+  http://pic.rmb.bdstatic.com/bjh/333.jpeg
+  folder333:
+    http://pic.rmb.bdstatic.com/bjh/333.jpeg
+    folder3333:
+      http://pic.rmb.bdstatic.com/bjh/333.jpeg
+`)]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),L=e("ul",null,[e("li",null,[t("It is not recommended to write like "),e("strong",null,[e("code",null,"2.jpeg, 5.jpeg")]),t(", it is recommended to go to that layer of folders to add links according to the format, not like the following error example to write")])],-1),P=e("h5",{id:"incorrect-usage",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#incorrect-usage","aria-hidden":"true"},"#"),t(),e("strong",null,"incorrect usage")],-1),X=e("div",{class:"language-text line-numbers-mode","data-ext":"text"},[e("pre",{class:"language-text"},[e("code",null,`folder22:
+  http://pic.rmb.bdstatic.com/bjh/222.jpeg
+  folder222:
+    http://pic.rmb.bdstatic.com/bjh/222.jpeg
+    folder2222:
+      http://pic.rmb.bdstatic.com/bjh/2222.jpeg
+      folder22222:
+        http://pic.rmb.bdstatic.com/bjh/2222.jpeg
+        folder222222:
+          http://pic.rmb.bdstatic.com/bjh/2222.jpeg
+    http://pic.rmb.bdstatic.com/bjh/2.jpeg
+        http://pic.rmb.bdstatic.com/bjh/5.jpeg
+`)]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),H=e("h5",{id:"correct-usage",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#correct-usage","aria-hidden":"true"},"#"),t(),e("strong",null,"correct usage")],-1),J=e("p",null,[t("When files like "),e("strong",null,[e("code",null,"2.jpeg, 5.jpeg")]),t(" are added, they are added to the corresponding directory, which correspond to the two folders "),e("code",null,"folder222 and folder22222"),t(" respectively")],-1),Z=e("div",{class:"language-text line-numbers-mode","data-ext":"text"},[e("pre",{class:"language-text"},[e("code",null,`folder22:
+  http://pic.rmb.bdstatic.com/bjh/222.jpeg
+  folder222:
+    http://pic.rmb.bdstatic.com/bjh/222.jpeg
+    http://pic.rmb.bdstatic.com/bjh/2.jpeg
+    folder2222:
+      http://pic.rmb.bdstatic.com/bjh/2222.jpeg
+      folder22222:
+        http://pic.rmb.bdstatic.com/bjh/2222.jpeg
+        http://pic.rmb.bdstatic.com/bjh/5.jpeg
+        folder222222:
+          http://pic.rmb.bdstatic.com/bjh/2222.jpeg
+`)]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),q=e("div",{class:"language-text line-numbers-mode","data-ext":"text"},[e("pre",{class:"language-text"},[e("code",null,`https://jsd.nn.ci/gh/alist-org/alist/README.md
+document
+  Readme.md:https://pic.rmb.bdstatic.com/bjh/说明.md
+  Doc:
+    document.doc:https://pic.rmb.bdstatic.com/bjh/测试文档.doc
+  pdf:
+    pdf2.pdf:https://pic.rmb.bdstatic.com/bjh/测试.pdf
+  Excel:
+    Excel3.xlsx:https://pic.rmb.bdstatic.com/bjh/测试表格.xlsx 
+video:
+  https://pic.rmb.bdstatic.com/bjh/222.jpeg
+  TV drama:
+    https://pic.rmb.bdstatic.com/bjh/222.pm4
+  Movie:
+    https://pic.rmb.bdstatic.com/bjh/222.flv
+music
+  https://pic.rmb.bdstatic.com/bjh/222.jpeg
+  classical
+    https://pic.rmb.bdstatic.com/bjh/222.mp3
+  Light music:
+    https://pic.rmb.bdstatic.com/bjh/222.flac
+    https://pic.rmb.bdstatic.com/bjh/222.wav
+software
+  https://pic.rmb.bdstatic.com/bjh/222.jpeg
+  crack:
+    xxexe.exe:252525:https://pic.rmb.bdstatic.com/bjh/222.exe
+    xxinstall.zip:259555:https://pic.rmb.bdstatic.com/bjh/222.zip
+  IPA:
+    GPT.ipa:https://pic.rmb.bdstatic.com/bjh/222.ipa
+  APK:
+    Ps.apk:https://pic.rmb.bdstatic.com/bjh/222.apk
+`)]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),$=e("p",null,null,-1),ee=h('<h2 id="tips" tabindex="-1"><a class="header-anchor" href="#tips" aria-hidden="true">#</a> <strong>Tips</strong></h2><ol><li>If you click download and find that there is no jump to download but preview, it is because it is determined according to your original link. For example, your original link is download and it will jump to download</li><li>If you have the same file in a folder, it will be merged and only one will be displayed. By default, the preview is used to view and download your upper (front) or top file. <ul><li>Tips: Do not create two files/folders with the same name in the same directory at the same time~ Just like you create two files/folders with the same name in the resource manager, you will be reminded not to repeat</li></ul></li><li>If you write two folders with the same name in the same directory when you add them, they will be merged into one folder, but the files in the lower (lower) folder will not be displayed after the merge, but you can view them through the browser Address bar access (refer to the previous tip)</li><li>By default, the sorting is based on what you fill in from top to bottom, which is the same as <code>alist-alias</code></li></ol><h3 id="the-default-download-method-used" tabindex="-1"><a class="header-anchor" href="#the-default-download-method-used" aria-hidden="true">#</a> <strong>The default download method used</strong></h3>',3);function te(ne,ie){const c=d("ExternalLinkIcon"),a=d("Badge"),r=d("Tabs"),o=d("Mermaid");return m(),b("div",null,[v,e("div",g,[f,e("p",null,[j,t(" What is it? It is used to mount a single file link. For details, you can check "),e("strong",null,[t("→→→"),e("a",_,[t("Original Requirements Description"),l(c)]),t("←←←")])])]),x,w,y,l(r,{id:"17",data:[{id:"1"},{id:"2"},{id:"3"},{id:"4"},{id:"5"}],"tab-id":"UrlTree"},{title0:n(({value:i,isActive:s})=>[t("1")]),title1:n(({value:i,isActive:s})=>[t("2")]),title2:n(({value:i,isActive:s})=>[t("3")]),title3:n(({value:i,isActive:s})=>[t("4")]),title4:n(({value:i,isActive:s})=>[t("5")]),tab0:n(({value:i,isActive:s})=>[l(a,{text:"1",type:"info",vertical:"middle"}),t("The simplest usage, just write the link directly"),k,A,E,T,I]),tab1:n(({value:i,isActive:s})=>[l(a,{text:"2",type:"info",vertical:"middle"}),t("While there are files, you can also create folders ^the first code example^, or you can create multiple folders ^the second code example^"),D,M,R,B,z,U,N,S]),tab2:n(({value:i,isActive:s})=>[l(a,{text:"3",type:"info",vertical:"middle"}),t("You can rename the file resource, in fact, the downloaded one is still the original one, and you can fill in the file size"),C,O,W,F,Q]),tab3:n(({value:i,isActive:s})=>[l(a,{text:"4",type:"info",vertical:"middle"}),t("Matryoshka doll usage of multiple levels of directories"),V,Y,G,K,L,P,X,H,J,Z]),tab4:n(({value:i,isActive:s})=>[l(a,{text:"5",type:"info",vertical:"middle"}),t("All integrated example usages include conventional, classified, rename, and marked file size"),q,$]),_:1}),ee,l(o,{id:"mermaid-157",code:"eJydkE1vwjAMhu/8CguutKztAC3SNmniuNO0aYeKQz6cJSJtuiSlVOLHr7QFscMkNF8S2+/zynYURZOgg0ECn0pzBcI2pbFUQIFBWQHaQ+1RAGtBoKS1Cc+TEySNbbiiLsD7ywS68KE1CDQBqY0hM8bk3Adnd0hmcrUa/1GjRVAkrQ5zbo11XU/KazwdcSnXy/Xy4pBl2W+H++owYDX7crRSoAUmkMO2r9Kkf7AUY5pnd+mWEOJtgdxQ7x+7OI4LHWmadzs6wO9aVwWWYXDphRuUcKHOwz2sBkGSG8upgcrZQ7uNYmoCupIGvcc4evrDliX55nzkHoSPt9dbYW40352uPI3jhXCd1vkFt0Vhy1iFwswaZILuo8p2wnZ6xbB/MPxm5gdtZr9x"})])}const le=u(p,[["render",te],["__file","UrlTree.html.vue"]]);export{le as default};
